712664a02a3fc7a0ebf8dbae3a7746bca4dd09cf
Sebastijonas Valaitis <d.dizya@gmail.com>
2025-02-19 13:07:22 +0200

    Įkeliu kuro kainas prie car_task

---
+++ car_task/fuel_prices.py
@@ -0,0 +1,12 @@
+"""
+Here are actual fuels prices for ___
+"""
+PRICES = {
+    "gasoline": 5.55,
+    "petrol": 5.55,
+    "diesel": 4.44,
+    "gas": 1.20,
+    "biodiesel": 5.00,
+    "ethanol": 6.00,
+    "electricity": 0.09,
+}
